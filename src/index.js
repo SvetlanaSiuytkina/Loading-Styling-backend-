512b57159dfa306123e451b2d320aa31ad83a6cf
SVETA <svetadan1234@mail.ru>
2026-09-02 17:30:00 +0500

    corrected

--- src/index.js
+++ src/index.js
@@ -44,5 +44,5 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`сервер запустился на \${port}`);
+  console.log(`сервер запустился на ${port}`);
 });
