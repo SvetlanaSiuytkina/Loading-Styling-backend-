ac0d60e8357737d19fc7788bd91b2d3d1fef3228
SVETA <svetadan1234@mail.ru>
2026-09-07 17:03:55 +0500

    corrected

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ const router = new Router();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
